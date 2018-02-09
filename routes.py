a1197106c8d08ec213a46e17a992dc2f0b5d8b9c
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-09 06:32:35 -0500

    Delete routes.py

--- routes.py
+++
@@ -1,8 +0,0 @@
-#Contains all the routes or pages or views
-from flask import Flask,url_for,request,render_template
-from main import app
-
-#server
-@app.route('/')
-def hello():
-    return render_template('hello.html')

5b92a1c8164482d2579319ca79fc46f6ad6834c8
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-09 04:07:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return render_template('try.html')
+from routes import *
 
 if __name__ == '__main__':
   app.run()

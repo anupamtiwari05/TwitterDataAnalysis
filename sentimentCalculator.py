633c6999ea60d39319692e8eeccf86acc0f0ca17
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-09 15:23:53 -0500

    Update sentimentCalculator.py

--- sentimentCalculator.py
+++ sentimentCalculator.py
@@ -13,7 +13,7 @@ class tweetsSenti:
     def searchTweets(self, q):
        # from pandas import pandas as pd
         from twitter import Twitter, OAuth, TwitterHTTPError
-        from pandas.io.json import json_normalize
+       # from pandas.io.json import json_normalize
         
         ACCESS_TOKEN = '136600388-9iihe7SFq8nZUOL5GjxoZlPbxW2MYcScWlZ6sD3a'
         ACCESS_SECRET = 'ScmAR4iYHCxuPHhYMifirTK0h2Jhdqt1p10uoz9lHTshT'

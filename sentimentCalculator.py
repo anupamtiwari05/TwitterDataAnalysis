d79875bc8570d5ae77e4c45dd84127a012d73bde
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-09 15:16:48 -0500

    Update sentimentCalculator.py

--- sentimentCalculator.py
+++ sentimentCalculator.py
@@ -6,14 +6,29 @@ Created on Mon Nov 13 20:45:47 2017
 """
 
 class tweetsSenti:
-    
-        
+
     def __init__(self, **kwargs):
         return super().__init__(**kwargs)
 
     def searchTweets(self, q):
-        from twitter import Twitter,  OAuth, TwitterHTTPError
         import pandas as pd
+        from twitter import Twitter, OAuth, TwitterHTTPError
         from pandas.io.json import json_normalize
         
-        return q + 'Nitesh'
+        ACCESS_TOKEN = '136600388-9iihe7SFq8nZUOL5GjxoZlPbxW2MYcScWlZ6sD3a'
+        ACCESS_SECRET = 'ScmAR4iYHCxuPHhYMifirTK0h2Jhdqt1p10uoz9lHTshT'
+        consumer_key = 'bto0MsRvjjfkrl4QpndjaUneg'
+        consumer_secret = '5zr7Xr9y4AbKgUCuWRmQGaMvizwg48HpVeyjbSZC4j350rIYPF'
+    
+        oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, consumer_key, consumer_secret)
+        twitter = Twitter(auth=oauth)
+        #q = 'modi'
+        count = 100
+        
+        search_results = twitter.search.tweets(q=q,count = count)
+        Original_status_df = json_normalize(search_results,['statuses'])
+        Original_status_df = pd.DataFrame(Original_status_df)
+        min_id = min(Original_status_df['id'])
+        max_id = max(Original_status_df['id'])
+
+        return 'Success'

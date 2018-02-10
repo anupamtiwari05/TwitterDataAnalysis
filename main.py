454a98f0672aa2c6a291732c4b92f5ee5892f4f9
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-10 01:53:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-WSGIApplicationGroup %{GLOBAL}
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 

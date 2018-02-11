57189fec4fcd895ee468612d57ea7d6ff8508396
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-10 20:50:01 -0500

    Update sentimentCalculator.py

--- sentimentCalculator.py
+++ sentimentCalculator.py
@@ -6,13 +6,13 @@ Created on Mon Nov 13 20:45:47 2017
 """
 
 class tweetsSenti:
-    import numpy as np
-    import pandas as pd
+    
     def __init__(self, **kwargs):
         return super().__init__(**kwargs)
 
     def searchTweets(self, q):
-        
+        import numpy as np
+        import pandas as pd
         from twitter import Twitter, OAuth, TwitterHTTPError
        # from pandas.io.json import json_normalize
         

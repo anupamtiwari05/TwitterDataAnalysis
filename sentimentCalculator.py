4f9d86ceff82a260b17be314761dba0ca8fa4a3d
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-14 01:29:39 -0500

    Update sentimentCalculator.py

--- sentimentCalculator.py
+++ sentimentCalculator.py
@@ -362,7 +362,8 @@ def worldMap(polarity,country_code):
         )
     ]
     world_map_id = ['World_Map']
+    data =json.dumps(world_map_id)
     #cls=plotly.plotly.utils.PlotlyJSONEncoder
     #world_map_json = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
     #return cls, world_map_id
-    return 'success'
+    return data

9ed1d73bf7c280e0df119473a4ba75a59ed4bfae
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-18 17:39:19 -0500

    Update views.py

--- views.py
+++ views.py
@@ -6,7 +6,6 @@ Created on Mon Nov 13 20:45:47 2017
 """
 
 from flask import render_template,url_for, flash, redirect, request
-from flask import Markup
 from main import app
 from sentimentCalculator import tweetsSenti
 
@@ -19,10 +18,10 @@ def hello():
         return render_template('hello.html')
     elif request.method=='POST':
         twitterHandle = request.form['twitterhandle']
-        obj=tweetsSenti()
-        world_map_string, world_map_ids,us_map_string, us_map_ids, country_tweets_count, world_country_df = obj.searchTweets(twitterHandle)
-            
 
-        return render_template('hello.html', worldPlot = world_map_string,
-                              world_map_ids = world_map_ids, usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
+        obj=tweetsSenti()
+
+        world_map_string, world_map_ids, us_map_string, us_map_ids, country_tweets_count, world_country_df = obj.searchTweets(twitterHandle)
+            
+        return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids, usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
                               country_tweets_count = country_tweets_count, world_country_df = world_country_df)

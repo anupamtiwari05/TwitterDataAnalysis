97ff63d4402f863c6eb45ed7ae980dd964c69908
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-23 16:46:52 -0500

    Update views.py

--- views.py
+++ views.py
@@ -23,19 +23,59 @@ def hello():
         countFileR=open(fullPath,'r')
         count = countFileR.read()
         countFileR.close()
-        return render_template('hello.html', likesCount=count, likeButtonColor='cornflowerblue',likeButtonValue='Like')
+        return render_template('hello.html',likesCount=count, likeButtonColor='cornflowerblue',likeButtonValue='Like')
     elif request.method=='POST':
         twitterHandle = request.form['twitterhandle']
+        likeButtonValue = request.form['hiddenButtonValue']
+        hiddenQueryText = request.form['hiddenQueryValue']
         
-        obj=tweetsSenti()
+        if likeButtonValue == 'Like':
+            likeButtonColor='cornflowerblue'
+        elif likeButtonValue == 'UnLike':
+            likeButtonColor='mediumseagreen'
 
-        world_map_string, world_map_ids, us_map_string, us_map_ids, world_tweets_count, world_country_df,country_tweets_count, summary_df_Country = obj.searchTweets(twitterHandle)
-        if(world_map_string==""):
-            return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids, usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
+      
+        path=os.getcwd()
+        fullPath=os.path.join(path,'Databases\\LikesCount.txt')
+        countFileR=open(fullPath,'r')
+        count = countFileR.read()
+        countFileR.close()
+        likeButtonColor=''
+        
+
+        if hiddenQueryText == '':
+            newCount=0
+            likeButtonColor=''
+            if likeButtonValue=="UnLike":
+                newCount= int(count) + 1
+                likeButtonColor='mediumseagreen'
+                likeButtonValue='UnLike'
+            elif likeButtonValue=="Like":
+                newCount= int(count) - 1
+                likeButtonColor='cornflowerblue'
+                likeButtonValue='Like'
+             
+        countFileW = open(fullPath,'w')
+        if newCount == 0:
+            countFileW.write(count)
+        elif newcount != 0:
+            countFileW.write(count)
+        
+        countFileW.close()
+
+        if twitterHandle!='' and hiddenQueryText!='':
+           obj=tweetsSenti()
+           world_map_string, world_map_ids, us_map_string, us_map_ids, world_tweets_count, world_country_df,country_tweets_count, summary_df_Country, bar_string, bar_ids = obj.searchTweets(twitterHandle)
+           if(world_map_string==""):
+              return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids, usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
                               world_tweets_count = world_tweets_count, world_country_df = world_country_df,country_tweets_count=country_tweets_count, summary_df_Country = summary_df_Country,
-                              exception = "Raise Exception")
+                              bar_string = bar_string, bar_ids = bar_ids, exception = "Raise Exception", likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
+           else:
+              return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids,
+                                  usaMapPlot = us_map_string, usa_map_ids = us_map_ids, world_tweets_count = world_tweets_count, world_country_df = world_country_df,
+                                  country_tweets_count=country_tweets_count, summary_df_Country = summary_df_Country,
+                                  barPlot = bar_string, bar_ids = bar_ids, likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
+        elif(likeButtonValue!=""):
+            return render_template('hello.html', likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
         else:
-            return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids,
-                                  usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
-                                  world_tweets_count = world_tweets_count, world_country_df = world_country_df,
-                                 country_tweets_count=country_tweets_count, summary_df_Country = summary_df_Country)
+            return render_template('hello.html', likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)

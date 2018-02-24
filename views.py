4e06b36ce870ad4b7e92373bf403d313dba884e7
Nitesh Sharma <35751229+nfSharmagit@users.noreply.github.com>
2018-02-23 22:42:07 -0500

    Update views.py

--- views.py
+++ views.py
@@ -15,6 +15,14 @@ import os
 # from markupsafe import Markup
 # index view function suppressed for brevity
 
+#@app.route('/')
+#def yo():
+#	#creating a variable for create page, method name for 'create' should be same
+#	createLink = '<a href=' + url_for('hello') + '>Click here to enter the website</a>'
+#	return '<html><head>Home Page</head><body>'+ createLink +'</body></html>'
+
+
+
 @app.route('/', methods=['GET','POST'])
 def hello():
     if request.method=='GET':
@@ -23,58 +31,49 @@ def hello():
         countFileR=open(fullPath,'r')
         count = countFileR.read()
         countFileR.close()
-        return render_template('hello.html',likesCount=count, likeButtonColor='cornflowerblue',likeButtonValue='Like')
+        return render_template('hello.html',likesCount = count)
     elif request.method=='POST':
         twitterHandle = request.form['twitterhandle']
-        likeButtonValue = request.form['hiddenButtonValue']
-        hiddenQueryText = request.form['hiddenQueryValue']
-        
-        if likeButtonValue == 'Like':
-            likeButtonColor='cornflowerblue'
-        elif likeButtonValue == 'UnLike':
-            likeButtonColor='mediumseagreen'
-
-      
         path=os.getcwd()
         fullPath=os.path.join(path,'Databases\\LikesCount.txt')
         countFileR=open(fullPath,'r')
         count = countFileR.read()
         countFileR.close()
-        
 
-        if hiddenQueryText == '':
-            newCount=0
-            likeButtonColor=''
-            if likeButtonValue=="UnLike":
-                newCount= int(count) + 1
-                likeButtonColor='mediumseagreen'
-                likeButtonValue='UnLike'
-            elif likeButtonValue=="Like":
-                newCount= int(count) - 1
-                likeButtonColor='cornflowerblue'
-                likeButtonValue='Like'
-             
-        countFileW = open(fullPath,'w')
-        if newCount == 0:
-            countFileW.write(count)
-        elif newCount != 0:
-            countFileW.write(str(newCount))
-        
-        countFileW.close()
-
-        if twitterHandle!='' and hiddenQueryText!='':
-           obj=tweetsSenti()
-           world_map_string, world_map_ids, us_map_string, us_map_ids, world_tweets_count, world_country_df,country_tweets_count, summary_df_Country, bar_string, bar_ids = obj.searchTweets(twitterHandle)
-           if(world_map_string==""):
-              return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids, usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
+        obj=tweetsSenti()
+        world_map_string, world_map_ids, us_map_string, us_map_ids, world_tweets_count, world_country_df,country_tweets_count, summary_df_Country, bar_string, bar_ids = obj.searchTweets(twitterHandle)
+        if(world_map_string==""):
+            return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids, usaMapPlot = us_map_string, usa_map_ids = us_map_ids, 
                               world_tweets_count = world_tweets_count, world_country_df = world_country_df,country_tweets_count=country_tweets_count, summary_df_Country = summary_df_Country,
-                              bar_string = bar_string, bar_ids = bar_ids, exception = "Raise Exception", likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
-           else:
-              return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids,
+                              bar_string = bar_string, bar_ids = bar_ids, exception = "Raise Exception", likesCount = count)
+        else:
+            return render_template('hello.html', worldPlot = world_map_string, world_map_ids = world_map_ids,
                                   usaMapPlot = us_map_string, usa_map_ids = us_map_ids, world_tweets_count = world_tweets_count, world_country_df = world_country_df,
                                   country_tweets_count=country_tweets_count, summary_df_Country = summary_df_Country,
-                                  barPlot = bar_string, bar_ids = bar_ids, likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
-        elif(likeButtonValue!=""):
-            return render_template('hello.html', likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
-        else:
-            return render_template('hello.html', likesCount = newCount,likeButtonColor = likeButtonColor,likeButtonValue = likeButtonValue)
+                                  barPlot = bar_string, bar_ids = bar_ids, likesCount = count)
+       
+
+@app.route('/likeButtonAction', methods=['POST'] )
+def likeButton():
+     value = request.form['hiddenButtonValue']
+     path=os.getcwd()
+     fullPath=os.path.join(path,'Databases\\LikesCount.txt')
+     countFileR=open(fullPath,'r')
+     count = countFileR.read()
+     countFileR.close()
+
+     newCount = 0
+     if value =="like":
+        newCount= int(count) + 1
+     elif value=="dislike":
+        newCount= int(count) - 1
+         
+     countFileW = open(fullPath,'w')
+     if newCount == 0:
+         countFileW.write(count)
+     elif newCount != 0:
+         countFileW.write(str(newCount))
+        
+     countFileW.close() 
+
+     return "",204   
